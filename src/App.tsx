3c2cacadc68d0641516fcfe24ed70c5514b77164
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-01-09 13:47:21 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { listRestaurants } from './graphql/queries'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Query query={gql(listRestaurants)}>
+          {({ data }) => {
+            return (
+              <div>
+                <h1>Hello World</h1>
+                {JSON.stringify(data)}
+              </div>
+            )
+          }}
+        </Query>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Amplify from 'aws-amplify'
+import { ApolloProvider } from 'react-apollo'
+import { AWSAppSyncClient, AUTH_TYPE } from 'aws-appsync'
+import { Rehydrated } from 'aws-appsync-react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App'
+import config from './aws-exports'
+import * as serviceWorker from './serviceWorker'
+
+Amplify.configure(config)
+
+const client = new AWSAppSyncClient({
+  url: config.aws_appsync_graphqlEndpoint,
+  region: config.aws_appsync_region,
+  auth: {
+    apiKey: config.aws_appsync_apiKey,
+    type: config.aws_appsync_authenticationType as AUTH_TYPE,
+  },
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+process.env.NODE_ENV === 'production'
+  ? serviceWorker.register()
+  : serviceWorker.unregister()
